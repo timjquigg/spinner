60997322c73d34256589cbc582fbc35063aebca3
Tim Quigg <timjquigg@gmail.com>
2022-10-25 14:11:07 -0600

    refactored spinner1 to spinner2

--- spinner1.js
+++ spinner1.js
@@ -1,20 +1,40 @@
-/*
-Let's practice some more with setTimeout, this time creating a low-fidelity loading spinner.
- */
+setTimeout(() => {
+}, 100);
 
-const spinner = () => {
-  const characters = ['|', '/', '-', '\\','|', '/', '-', '\\', '|'];
-  let delay = 100;
-  for (const char of characters) {
-    setTimeout(() => {
-      process.stdout.write(`\r${char}   `);
-    }, delay);
-    delay += 200;
-  }
-  
-  setTimeout(() => {
-    process.stdout.write('\n');
-  }, delay);
-};
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
 
-spinner();
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   ');
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1700);
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, 1900);
